79db54a3ff3da29fd90441b692edf6574b689b42
Martin Bierbaum <martin.bierbaum@rub.de>
2017-12-31 17:12:04 +0100

    pylint tests.py

--- django_timesheet/timesheet/tests.py
+++ django_timesheet/timesheet/tests.py
@@ -9,18 +9,18 @@ from django_timesheet.timesheet.models import File, Task
 class TimesheetViews(TestCase):
 
     def test_create_file(self):
-        
+
         response = self.client.get(reverse('create_file'))
         self.assertEqual(response.status_code, 200)
 
-        response = self.client.post(reverse('create_file'), 
-            data = {'reference': 'foo'})
+        response = self.client.post(reverse('create_file'),
+            data={'reference': 'foo'})
 
-        
+
         file = File.objects.first()
         self.assertEqual(file.reference, 'foo')
         self.assertEqual(File.objects.count(), 1)
-        
+
         self.assertRedirects(response, file.get_absolute_url())
 
     def test_create_task(self):
@@ -30,17 +30,17 @@ class TimesheetViews(TestCase):
         response = self.client.get(reverse('create_task'))
         self.assertEqual(response.status_code, 200)
 
-        response = self.client.post(reverse('create_task'), 
-            data = {
+        response = self.client.post(reverse('create_task'),
+            data={
                 'file': file.pk,
                 'description': 'foo'
             })
-        
+
         task = Task.objects.first()
         self.assertEqual(task.description, 'foo')
         self.assertEqual(task.date, now().date())
         self.assertEqual(Task.objects.count(), 1)
-        
+
         self.assertRedirects(response, task.get_absolute_url())
 
         # Create task without file
