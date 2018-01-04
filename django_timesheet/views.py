abf26d08b1b54b56639ae0f2982c1206890cd437
Martin Bierbaum <martin.bierbaum@rub.de>
2018-01-04 18:29:55 +0100

    show running tasks on index.html

--- django_timesheet/views.py
+++ django_timesheet/views.py
@@ -1,6 +1,13 @@
 
 from django.views import generic
 
+from django_timesheet.timesheet.models import Task
+
 class HomePage(generic.TemplateView):
 
     template_name = 'django_timesheet/index.html'
+
+    def get_context_data(self, **kwargs):
+        qs = Task.objects.filter(timer__status__in=['running'])
+        kwargs['object_list'] = qs
+        return super().get_context_data(**kwargs)
