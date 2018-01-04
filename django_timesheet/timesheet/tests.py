d5433902ad521ad3889f51ad2576cd5a9df4e287
Martin Bierbaum <martin.bierbaum@rub.de>
2018-01-05 00:43:43 +0100

    remove plain create task view

--- django_timesheet/timesheet/tests.py
+++ django_timesheet/timesheet/tests.py
@@ -76,33 +76,6 @@ class TimesheetViews(TestCase):
 
         self.assertRedirects(response, file.get_absolute_url())
 
-    def test_create_task(self):
-
-        file = File.objects.create(reference='foo')
-
-        response = self.client.get(reverse('create_task'))
-        self.assertEqual(response.status_code, 200)
-
-        response = self.client.post(reverse('create_task'),
-            data={
-                'file': file.pk,
-                'description': 'foo'
-            })
-
-        task = Task.objects.first()
-        self.assertEqual(task.description, 'foo')
-        self.assertEqual(task.date, datetime.date.today())
-        self.assertEqual(Task.objects.count(), 1)
-
-        self.assertRedirects(response, task.get_absolute_url())
-
-        # Create task without file
-        response = self.client.post(reverse('create_task'),
-            data={'description': 'bar'})
-
-        task = Task.objects.get(description='bar')
-        self.assertIsNone(task.file)
-
     def test_create_task_from_file(self):
 
         file = File.objects.create()
--- django_timesheet/timesheet/urls.py
+++ django_timesheet/timesheet/urls.py
@@ -8,6 +8,5 @@ urlpatterns = [
     url(r'^file/create/$', views.FileCreateView.as_view(), name='create_file'),
     url(r'^file/(?P<pk>\d+)/$', views.FileUpdateView.as_view(), name='file'),
     url(r'^file/(?P<pk>\d+)/task/create/$', views.TaskCreateView.as_view(), name='create_task'),
-    url(r'^task/create/$', views.TaskCreateView.as_view(), name='create_task'),
     url(r'^task/(?P<pk>\d+)/$', views.TaskUpdateView.as_view(), name='task'),
 ]
