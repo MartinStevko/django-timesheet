0d46b6b5354d8805e047344840887efcbb28e2dc
Martin Bierbaum <martin.bierbaum@rub.de>
2018-01-08 14:15:56 +0100

    add reference to task filter

--- django_timesheet/timesheet/filters.py
+++ django_timesheet/timesheet/filters.py
@@ -3,5 +3,6 @@ import django_selector as ds
 
 class TaskFilter(ds.FilterSet):
 
+    reference = ds.CharFilter(name='file__reference', label='Aktenzeichen')
     from_date = ds.DateFilter(name='date', lookup_expr='gte', label='Von')
     to_date = ds.DateFilter(name='date', lookup_expr='lte', label='Bis')
