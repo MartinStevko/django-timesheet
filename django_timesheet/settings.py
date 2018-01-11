54f9bb8fa28c39d8c25e1f40c9d4afa4fd8c1b50
Martin Bierbaum <martin.bierbaum@rub.de>
2018-01-12 00:56:35 +0100

    rename static subfolder and edit settings

--- django_timesheet/settings.py
+++ django_timesheet/settings.py
@@ -56,9 +56,7 @@ ROOT_URLCONF = 'django_timesheet.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            os.path.join(BASE_DIR, 'django_timesheet/templates'),
-        ],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,6 +119,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "django_timesheet/static"),
-]
